39f609a8becd64c87c71c75e6294d62c280115e9
nehaahuja2021 <nehaahuja2021@gmail.com>
2021-09-15 11:25:14 +0200

    react text app

---
+++ src/components/Textform.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+export default function Textform(props) {
+   const handleUpClick = () => {
+      // console.log("Uppercase was Clicked")
+      let newText = text.toUpperCase()
+      setText(newText)
+   }
+   const handleLowClick = () => {
+      console.log("low click button was clicked")
+      let newText = text.toLowerCase()
+      setText(newText)
+   }
+   const clearText = () => {
+      let newText = ''
+      setText(newText)
+   }
+   const handleOnChange = (event) => {
+      console.log("On change")
+      setText(event.target.value)
+   }
+   const [text, setText] = useState('');
+   return (
+      <>
+         <div className="container">
+            <h3>{props.heading} </h3>
+            <div className="mb-3">
+               <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert toLowerCase</button>
+            <button className="btn btn-primary mx-2" onClick={clearText}>Clear Text</button>
+
+         </div>
+         <div className="container my-3">
+            <h4> Youe text summary</h4>
+            <p> {text.split(" ").length} Words and {text.length} characters</p>
+            <p>  {0.008 * text.split(" ").length} Minutes Read</p>
+            <p> Preview : {text}</p>
+         </div>
+      </>
+
+
+   )
+}
