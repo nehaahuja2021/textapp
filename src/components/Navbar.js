cd46ee15024d7bc0e9ae300455c10bf364d57abe
nehaahuja2021 <nehaahuja2021@gmail.com>
2021-09-18 07:13:36 +0200

    home deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,7 @@ export default function Navbar(props) {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
-                  <li className="nav-item">
-                     <Link className="nav-link active" aria-current="page" to href="/">Home</Link>
-                  </li>
+
                   <li className="nav-item">
                      <Link className="nav-link" to="/about">{props.About}</Link>
                   </li>
